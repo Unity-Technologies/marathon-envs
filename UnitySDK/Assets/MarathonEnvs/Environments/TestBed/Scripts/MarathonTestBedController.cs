a2fa88a39185d204abb289a59c461eece81ae1c3
Joe Booth <joe@joebooth.com>
2018-12-19 16:45:37 -0800

    fix testbed

---
+++ UnitySDK/Assets/MarathonEnvs/Environments/TestBed/Scripts/MarathonTestBedController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarathonTestBedController : MonoBehaviour
+{
+    [Tooltip("Action applied to each motor")]
+    /**< \brief Edit to manually test each motor (+1/-1)*/
+    public float[] Actions;
+
+    [Tooltip("Apply a random number to each action each framestep")]
+    /**< \brief Apply a random number to each action each framestep*/
+    public bool ApplyRandomActions = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
