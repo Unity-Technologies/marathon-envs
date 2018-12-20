67510c91dbc7c872c8ee395cb9ab376d36a89620
Joe Booth <joe@joebooth.com>
2018-12-19 22:54:13 -0800

    TestBet auto freeze head and/or butt

--- UnitySDK/Assets/MarathonEnvs/Environments/TestBed/Scripts/MarathonTestBedController.cs
+++ UnitySDK/Assets/MarathonEnvs/Environments/TestBed/Scripts/MarathonTestBedController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using MLAgents;
 using UnityEngine;
 
 public class MarathonTestBedController : MonoBehaviour
@@ -12,16 +14,48 @@ public class MarathonTestBedController : MonoBehaviour
     /**< \brief Apply a random number to each action each framestep*/
     public bool ApplyRandomActions = true;
 
+    public bool FreezeHead = false;
+    public bool FreezeHips = false;
+    bool _hasFrozen;
+
 
     // Start is called before the first frame update
     void Start()
     {
+
+    }
+    void FreezeBodyParts()
+    {
+
+        var marathonAgents = FindObjectsOfType<MarathonAgent>();
+        foreach (var agent in marathonAgents)
+        {
+            Rigidbody head = null;
+            Rigidbody butt = null;
+            switch (agent.name)
+            {
+                case "humanoid":
+                    _hasFrozen = true;
+                    var children = agent.GetComponentsInChildren<Rigidbody>();
+                    head = children.FirstOrDefault(x=>x.name=="head");
+                    butt = children.FirstOrDefault(x=>x.name=="butt");
+                    break;
+                default:
+                    break;
+            }
+            if (FreezeHead && head != null)
+                head.constraints = RigidbodyConstraints.FreezeAll;
+            if (FreezeHead && butt != null)
+                butt.constraints = RigidbodyConstraints.FreezeAll;
+
+        }
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (!_hasFrozen)
+            FreezeBodyParts();
     }
 }
