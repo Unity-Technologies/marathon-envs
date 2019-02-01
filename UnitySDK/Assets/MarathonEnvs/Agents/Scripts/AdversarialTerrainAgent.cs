c258eb15ddd40f9ddb90a42a2252453486fc4468
Joe Booth <joe@joebooth.com>
2019-01-31 21:41:42 -1000

    wip - setting up the environment to test on PC

---
+++ UnitySDK/Assets/MarathonEnvs/Agents/Scripts/AdversarialTerrainAgent.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MLAgents;
+using UnityEngine;
+
+public class AdversarialTerrainAgent : Agent {
+
+    Terrain terrain;
+	int lastSteps;
+	Agent _agent;
+
+
+	int _posXInTerrain;
+	int _posYInTerrain;
+	float[,] _heights;
+
+	int _heightIndex;
+	float _curHeight;
+	float _actionReward;
+
+	internal const float _maxHeight = 10f;
+	const float _midHeight = 5f;
+	float _mapScaleY;
+
+	public override void AgentReset()
+	{
+		// get start position
+		if (this.terrain == null)
+		{
+			var terrains = Terrain.activeTerrains;
+			this.terrain = terrains.FirstOrDefault(x=>x.gameObject.transform.parent == gameObject.transform.parent);
+		}
+		if (this._agent == null)
+			_agent = GetComponent<Agent>();
+        //print($"HeightMap {this.terrain.terrainData.heightmapWidth}, {this.terrain.terrainData.heightmapHeight}. Scale {this.terrain.terrainData.heightmapScale}. Resolution {this.terrain.terrainData.heightmapResolution}");
+        _mapScaleY = this.terrain.terrainData.heightmapScale.y;
+		// get the normalized position of this game object relative to the terrain
+        Vector3 tempCoord = (transform.position - terrain.gameObject.transform.position);
+        Vector3 coord;
+        coord.x = (tempCoord.x-1) / terrain.terrainData.size.x;
+        coord.y = tempCoord.y / terrain.terrainData.size.y;
+        coord.z = tempCoord.z / terrain.terrainData.size.z;
+        // get the position of the terrain heightmap where this game object is
+        _posXInTerrain = (int) (coord.x * terrain.terrainData.heightmapWidth); 
+        _posYInTerrain = (int) (coord.z * terrain.terrainData.heightmapHeight);
+        // we set an offset so that all the raising terrain is under this game object
+        int offset = 0 / 2;
+        // get the heights of the terrain under this game object
+        _heights = terrain.terrainData.GetHeights(_posXInTerrain-offset,_posYInTerrain-offset, 100,1);
+		_curHeight = _midHeight;
+		_heightIndex = 0;
+		_actionReward = 0f;
+
+        
+        // set the new height
+        // terrain.terrainData.SetHeights(_posXInTerrain-offset,_posYInTerrain-offset,_heights);
+        //_heights[0,0] = 0.1f/600f;
+        //this.terrain.terrainData.SetHeights(_posXInTerrain, _posYInTerrain, _heights);
+		ResetHeights();
+		SetNextHeight(0);
+		SetNextHeight(0);
+		SetNextHeight(0);
+		SetNextHeight(0);
+		SetNextHeight(0);
+		SetNextHeight(0);
+
+		lastSteps = 0;
+		//RequestDecision();
+	}
+
+	void ResetHeights()
+	{
+		for (int i = 0; i < _heights.Length; i++)
+		{
+			_heights[0,i] = _midHeight / _mapScaleY;
+		}
+        this.terrain.terrainData.SetHeights(_posXInTerrain, _posYInTerrain, _heights);
+        this.terrain.terrainData.SetHeights(_posXInTerrain, _posYInTerrain+1, _heights);
+	}
+
+	void SetNextHeight(int action)
+	{
+		float actionSize = 0f;
+		bool actionPos = (action-1) % 2 == 0;
+		if (action != 0)
+		{
+			actionSize = ((float)((action+1)/2)) * 0.1f;
+			_curHeight += actionPos ? actionSize : -actionSize;
+			if (_curHeight < 0) {
+				_curHeight = 0f;
+				actionSize = 0;
+				AddReward(-1f);
+			}
+			if (_curHeight > _maxHeight)
+			{
+				_curHeight = _maxHeight;
+				actionSize = 0;
+				AddReward(-1f);				
+			}
+		}
+
+		_heights[0,_heightIndex] = _curHeight / _mapScaleY;
+        // this.terrain.terrainData.SetHeights(_posXInTerrain, _posYInTerrain, _heights);
+        // this.terrain.terrainData.SetHeights(_posXInTerrain, _posYInTerrain+1, _heights);
+		for (int i = 0; i < terrain.terrainData.heightmapHeight; i++)
+		{
+	        this.terrain.terrainData.SetHeights(_posXInTerrain, i, _heights);
+		}
+
+		_heightIndex++;
+		_actionReward = actionSize;
+		// if (_actionReward != 0 && actionPos)
+		// 	_actionReward *= 2f; // boost reward for uphill
+	}
+	internal void OnNextMeter()
+	{
+		// AddReward(1);
+		// AddReward(_actionReward);
+		_actionReward = 0f;
+		RequestDecision();
+	}
+	internal void Terminate(float cumulativeReward)
+	{
+		if (this.IsDone())
+			return;
+		var maxReward = 1000f;
+		var agentReward = cumulativeReward;
+		agentReward = Mathf.Clamp(agentReward, 0f, maxReward);
+		var adverseralReward = maxReward - agentReward;
+		AddReward(adverseralReward);
+		Done();
+		// RequestDecision();
+	}
+
+	public override void CollectObservations()
+	{
+		var height = _curHeight / _maxHeight;
+		// add last agent distance
+		
+		int curSteps = 0;
+		if (_agent != null)
+			curSteps = _agent.GetStepCount();
+		var numberSinceLast = curSteps - lastSteps;
+		numberSinceLast = 1 - (numberSinceLast/1000);
+		lastSteps = curSteps;
+        AddVectorObs(numberSinceLast);
+        AddVectorObs(height);
+        AddVectorObs(_actionReward);
+	}
+	public override void AgentAction(float[] vectorAction, string textAction)
+	{
+		// each action is a descreate for height change
+		int action = (int)vectorAction[0];
+		SetNextHeight(action);
+	}
+}
---
+++ UnitySDK/Assets/MarathonEnvs/Agents/Scripts/AdversarialTerrainHopperAgent.cs
@@ -0,0 +1,191 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using MLAgents;
+public class AdversarialTerrainHopperAgent : MarathonAgent {
+
+    AdversarialTerrainAgent _adversarialTerrainAgent;
+    int _lastXPosInMeters;
+    float _pain;
+    bool _modeRecover;
+    Vector3 _centerOfMass;
+
+    public override void AgentReset()
+    {
+        base.AgentReset();
+
+        BodyParts["pelvis"] = GetComponentsInChildren<Rigidbody>().FirstOrDefault(x=>x.name=="torso");
+        BodyParts["foot"] = GetComponentsInChildren<Rigidbody>().FirstOrDefault(x=>x.name=="foot");
+
+        SetCenterOfMass();
+
+        if (_adversarialTerrainAgent == null)
+            _adversarialTerrainAgent = GetComponent<AdversarialTerrainAgent>();
+        _lastXPosInMeters = (int) BodyParts["foot"].transform.position.x;
+        _adversarialTerrainAgent.Terminate(GetCumulativeReward());
+
+        // set to true this to show monitor while training
+        Monitor.SetActive(true);
+
+        StepRewardFunction = StepRewardHopper101;
+        TerminateFunction = LocalTerminate;
+        ObservationsFunction = ObservationsDefault;
+        // OnTerminateRewardValue = -100f;
+        _pain = 0f;
+        _modeRecover = false;
+
+        base.SetupBodyParts();
+        SetCenterOfMass();
+    }
+
+    bool LocalTerminate()
+    {
+        int newXPosInMeters = (int) BodyParts["foot"].transform.position.x;
+        if (newXPosInMeters > _lastXPosInMeters) {
+            _adversarialTerrainAgent.OnNextMeter();
+            _lastXPosInMeters = newXPosInMeters;
+        }
+
+        SetCenterOfMass();
+        var xpos = _centerOfMass.x;
+        var terminate = false;
+        if (xpos < 4f && _pain > 1f)
+            terminate = true;
+        else if (xpos < 2f && _pain > 0f)
+            terminate = true;
+        if (terminate)
+            _adversarialTerrainAgent.Terminate(GetCumulativeReward());
+
+        return terminate;
+    }
+    public override void OnTerrainCollision(GameObject other, GameObject terrain) {
+        if (terrain.GetComponent<Terrain>() == null)
+            return;
+
+        switch (other.name.ToLowerInvariant().Trim())
+        {
+            case "thigh": // dm_hopper
+            case "pelvis": // dm_hopper
+                _pain += 5f;
+                NonFootHitTerrain = true;
+                _modeRecover = true;
+                break;
+            case "foot": // dm_hopper
+            case "calf": // dm_hopper
+                FootHitTerrain = true;
+                break;
+            default:
+                _pain += 5f;
+                NonFootHitTerrain = true;
+                _modeRecover = true;
+                break;
+        }
+    }
+
+
+    public override void AgentOnDone()
+    {
+    }    
+    void ObservationsDefault()
+    {
+        if (ShowMonitor) {
+        }
+        var pelvis = BodyParts["pelvis"];
+        AddVectorObs(pelvis.velocity);
+        AddVectorObs(pelvis.transform.forward); // gyroscope 
+        AddVectorObs(pelvis.transform.up);
+        
+        AddVectorObs(SensorIsInTouch);
+        JointRotations.ForEach(x=>AddVectorObs(x));
+        AddVectorObs(JointVelocity);
+        var foot = BodyParts["foot"];
+        AddVectorObs(foot.transform.position.y);
+
+        var xpos = pelvis.transform.position.x;
+        xpos -= 2f;
+        float fraction = (xpos - (Mathf.Floor(xpos*5)/5)) * 5;
+        float ypos = pelvis.transform.position.y;
+        List<Ray> rays = Enumerable.Range(0, 5*5).Select(x => new Ray(new Vector3(xpos+(x*.2f), AdversarialTerrainAgent._maxHeight, 0f), Vector3.down)).ToList();
+        List<float> distances = rays.Select
+            ( x=>
+                ypos - (AdversarialTerrainAgent._maxHeight - 
+                Physics.RaycastAll(x)
+                .OrderBy(y=>y.distance)
+                .FirstOrDefault()
+                .distance)
+            ).ToList();
+        if (Application.isEditor && ShowMonitor)
+        {
+            var view = distances.Skip(10).Take(20).Select(x=>x).ToList();
+            Monitor.Log("distances", view.ToArray());
+            var time = Time.deltaTime;
+            time *= agentParameters.numberOfActionsBetweenDecisions;
+            for (int i = 0; i < rays.Count; i++)
+            {
+                var distance = distances[i];
+                var origin = new Vector3(rays[i].origin.x, ypos,0f);
+                var direction = distance > 0 ? Vector3.down : Vector3.up;
+                var color = distance > 0 ? Color.yellow : Color.red;
+                Debug.DrawRay(origin, direction*Mathf.Abs(distance), color, time, false);
+            }
+        }        
+        AddVectorObs(distances);
+        AddVectorObs(fraction);
+    }
+
+
+    void SetCenterOfMass()
+    {
+        _centerOfMass = Vector3.zero;
+        float c = 0f;
+        var bodyParts = this.gameObject.GetComponentsInChildren<Rigidbody>();
+ 
+        foreach (var part in bodyParts)
+        {
+            _centerOfMass += part.worldCenterOfMass * part.mass;
+            c += part.mass;
+        }
+        _centerOfMass /= c;
+    }
+
+    float StepRewardHopper101()
+    {
+        // float heightPenality = GetHeightPenality(0.5f);
+        float uprightBonus = GetForwardBonus("pelvis");
+        float velocity = GetVelocity("pelvis");
+        float effort = GetEffort();
+        // var effortPenality = 1e-2f * (float)effort;
+        var effortPenality = 3e-1f * (float)effort;
+        var jointsAtLimitPenality = GetJointsAtLimitPenality() * 4;
+
+        //var uprightScaler = Mathf.Clamp(velocity,0,1);
+        //uprightBonus *= 0f;//uprightScaler;
+        if (_pain > 0f){
+            uprightBonus = 0f;
+        }
+        if (_modeRecover) {
+            uprightBonus = 0f;
+            effortPenality = 0f;
+        }
+
+        var reward = velocity
+            +uprightBonus
+            // -heightPenality
+            -effortPenality
+            -jointsAtLimitPenality
+            -_pain
+            ;
+        if (ShowMonitor) {
+            // var hist = new []{reward,velocity,uprightBonus,-heightPenality,-effortPenality}.ToList();
+            var hist = new []{reward, velocity, uprightBonus, -effortPenality, 
+            -jointsAtLimitPenality,
+            -_pain
+            }.ToList();
+            Monitor.Log("rewardHist", hist.ToArray().ToString());
+        }
+        _pain = 0f;
+        return reward;
+    }
+}
