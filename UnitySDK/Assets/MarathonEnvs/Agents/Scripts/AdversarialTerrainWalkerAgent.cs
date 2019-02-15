7395f485d9e1970d6aa56093aa815cb32a749122
Joe Booth <joe@joebooth.com>
2019-02-14 17:41:16 -0800

    adv walker

--- UnitySDK/Assets/MarathonEnvs/Agents/Scripts/AdversarialTerrainWalkerAgent.cs
+++ UnitySDK/Assets/MarathonEnvs/Agents/Scripts/AdversarialTerrainWalkerAgent.cs
@@ -1,1 +1,160 @@
-// AdversarialTerrainWalkerAgent.cs
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using MLAgents;
+public class AdversarialTerrainWalkerAgent : MarathonAgent {
+
+    AdversarialTerrainAgent _adversarialTerrainAgent;
+    int _lastXPosInMeters;
+    float _pain;
+    bool _modeRecover;
+    Vector3 _centerOfMass;
+
+    public override void AgentReset()
+    {
+        base.AgentReset();
+
+        BodyParts["pelvis"] = GetComponentsInChildren<Rigidbody>().FirstOrDefault(x => x.name == "torso");
+        BodyParts["left_thigh"] = GetComponentsInChildren<Rigidbody>().FirstOrDefault(x => x.name == "left_thigh");
+        BodyParts["right_thigh"] = GetComponentsInChildren<Rigidbody>().FirstOrDefault(x => x.name == "right_thigh");
+        BodyParts["right_foot"] = GetComponentsInChildren<Rigidbody>().FirstOrDefault(x => x.name == "right_foot");
+        BodyParts["left_foot"] = GetComponentsInChildren<Rigidbody>().FirstOrDefault(x => x.name == "left_foot");
+
+        SetCenterOfMass();
+
+        if (_adversarialTerrainAgent == null)
+            _adversarialTerrainAgent = GetComponent<AdversarialTerrainAgent>();
+        _lastXPosInMeters = (int) BodyParts["pelvis"].transform.position.x;
+        _adversarialTerrainAgent.Terminate(GetCumulativeReward());
+
+        // set to true this to show monitor while training
+        Monitor.SetActive(true);
+
+        StepRewardFunction = StepRewardWalker106;
+        TerminateFunction = LocalTerminate;
+        ObservationsFunction = ObservationsDefault;
+        // OnTerminateRewardValue = -100f;
+        _pain = 0f;
+        _modeRecover = false;
+
+        base.SetupBodyParts();
+        SetCenterOfMass();
+    }
+
+    bool LocalTerminate()
+    {
+        int newXPosInMeters = (int) BodyParts["pelvis"].transform.position.x;
+        if (newXPosInMeters > _lastXPosInMeters) {
+            _adversarialTerrainAgent.OnNextMeter();
+            _lastXPosInMeters = newXPosInMeters;
+        }
+
+        SetCenterOfMass();
+        var xpos = _centerOfMass.x;
+        var terminate = false;
+        if (xpos < 4f && _pain > 1f)
+            terminate = true;
+        else if (xpos < 2f && _pain > 0f)
+            terminate = true;
+        if (terminate)
+            _adversarialTerrainAgent.Terminate(GetCumulativeReward());
+
+        return terminate;
+    }
+    public override void OnTerrainCollision(GameObject other, GameObject terrain) {
+        if (terrain.GetComponent<Terrain>() == null)
+            return;
+
+        switch (other.name.ToLowerInvariant().Trim())
+        {
+            case "pelvis": // dm_hopper
+                _pain += 5f;
+                NonFootHitTerrain = true;
+                _modeRecover = true;
+                break;
+            case "right_leg": // dm_walker
+            case "left_leg": // dm_walker
+            case "right_foot": // dm_walker
+            case "left_foot": // dm_walker
+                FootHitTerrain = true;
+                break;
+            default:
+                _pain += 5f;
+                NonFootHitTerrain = true;
+                _modeRecover = true;
+                break;
+        }
+    }
+
+
+    public override void AgentOnDone()
+    {
+    }    
+    void ObservationsDefault()
+    {
+        var pelvis = BodyParts["pelvis"];
+        Vector3 normalizedVelocity = this.GetNormalizedVelocity(pelvis.velocity);
+        AddVectorObs(normalizedVelocity);
+        AddVectorObs(pelvis.transform.forward); // gyroscope 
+        AddVectorObs(pelvis.transform.up);
+
+        AddVectorObs(SensorIsInTouch);
+        JointRotations.ForEach(x => AddVectorObs(x));
+        AddVectorObs(JointVelocity);
+        // AddVectorObs(new []{
+        //     this.GetNormalizedPosition(BodyParts["left_foot"].transform.position).y,
+        //     this.GetNormalizedPosition(BodyParts["right_foot"].transform.position).y
+        // });
+
+        (List<float> distances, float fraction) = 
+            _adversarialTerrainAgent.GetDistances2d(
+                pelvis.transform.position, ShowMonitor);
+   
+        AddVectorObs(distances);
+        AddVectorObs(fraction);
+    }
+
+
+    void SetCenterOfMass()
+    {
+        _centerOfMass = Vector3.zero;
+        float c = 0f;
+        var bodyParts = this.gameObject.GetComponentsInChildren<Rigidbody>();
+ 
+        foreach (var part in bodyParts)
+        {
+            _centerOfMass += part.worldCenterOfMass * part.mass;
+            c += part.mass;
+        }
+        _centerOfMass /= c;
+    }
+
+    float StepRewardWalker106()
+    {
+        float uprightBonus = GetDirectionBonus("pelvis", Vector3.forward, 1f);
+        uprightBonus = Mathf.Clamp(uprightBonus, 0f, 1f);
+        float velocity = Mathf.Clamp(GetNormalizedVelocity("pelvis").x, 0f, 1f);
+        float effort = 1f - GetEffortNormalized();
+
+        if (ShowMonitor)
+        {
+            var hist = new[] {velocity, uprightBonus, effort}.ToList();
+            Monitor.Log("rewardHist", hist.ToArray(), displayType: Monitor.DisplayType.INDEPENDENT);
+        }
+
+        uprightBonus *= 0.1f;
+        velocity *= 0.45f;
+        if (velocity >= .45f)
+            effort *= 0.45f;
+        else
+            effort *= velocity;
+
+        var reward = velocity
+                     + uprightBonus
+                     + effort;
+
+        return reward;
+    }
+}
