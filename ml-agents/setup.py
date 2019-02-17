1942382c5b778a08535c5a9307fe18d0b99f7ec6
Joe Booth <joe@joebooth.com>
2019-02-17 10:45:04 -0800

    trained models

--- ml-agents/setup.py
+++ ml-agents/setup.py
@@ -28,16 +28,17 @@ setup(
     packages=find_packages(exclude=['tests', 'tests.*', '*.tests', '*.tests.*']),  # Required
 
     install_requires=[
-        'tensorflow>=1.7,<1.8',
+        # 'tensorflow>=1.7,<1.8',
         'Pillow>=4.2.1',
         'matplotlib',
-        'numpy>=1.13.3,<=1.14.5',
-        'jupyter',
-        'pytest>=3.2.2,<4.0.0',
+        # 'numpy>=1.13.3,<=1.14.5',
+        # 'jupyter',
+        # 'pytest>=3.2.2,<4.0.0',
         'docopt',
         'pyyaml',
         'protobuf>=3.6,<3.7',
-        'grpcio>=1.11.0,<1.12.0'],
+        'grpcio>=1.11.0,<1.12.0'
+        ],
 
     python_requires=">=3.6,<3.7",
 
